8f7e6cf5c1dcae62f01ad02b06725fe1c92e7bd8
hassandaru <hassandaru@gmail.com>
2022-11-28 15:32:04 +1000

    adding lists and loading data from lists and mock data.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:layout_practice/location_detail.dart';
+// import 'package:layout_practice/location_detail.dart';
 import 'package:layout_practice/models/location.dart';
 import 'package:layout_practice/mocks/mock_location.dart';
-
+import 'location_list.dart';
 
 void main() {
-   final Location mockLocation = MockLocation.FetchAny();
-   return runApp( MaterialApp(home: LocationDetail(mockLocation)));
+   // final Location mockLocation = MockLocation.FetchAny();
+   // return runApp( MaterialApp(home: LocationDetail(mockLocation)));
+   final mockLocations = MockLocation.fetchAll();
+
+   return runApp(MaterialApp(home: LocationList(mockLocations)));
 }
 
