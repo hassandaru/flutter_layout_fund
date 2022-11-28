98ab0a53c50512b3f85794e41c1ec43149426774
hassandaru <hassandaru@gmail.com>
2022-11-28 14:15:54 +1000

    complete adding styles and custom fonts to project

--- lib/styles.dart
+++ lib/styles.dart
@@ -5,6 +5,10 @@ class Styles {
   static const _textSizeDefault = 16.0;
   static final Color _textColorStrong = _hexToColor('000000');
   static final Color _textColorDefault = _hexToColor('666666');
+  static final String _fontNameDefault = 'Muli';
+  static final navBarTitle = TextStyle(
+    fontFamily: _fontNameDefault,
+  );
   static final headerLarge =  TextStyle(
     // fontFamily: _fontNameDefault,
   fontSize: _textSizeLarge,
